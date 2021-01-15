f2918dbb054d0b6d1f96fb9ed77e8db73d039f3c
s3790768 <s3790768@student.rmit.edu.au>
2021-01-15 18:46:19 +1100

    Create parcel from model

--- src/backend/src/main/java/controllers/CreateNewParcel.kt
+++ src/backend/src/main/java/controllers/CreateNewParcel.kt
@@ -4,6 +4,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.cloud.FirestoreClient
 import io.javalin.http.Context
 import io.javalin.http.Handler
+import models.Parcel
 import java.lang.Exception
 
 class CreateNewParcel: Handler {
@@ -11,7 +12,6 @@ class CreateNewParcel: Handler {
     override fun handle(context: Context) {
         val db = FirestoreClient.getFirestore()
         val docRef = db.collection("jobs")
-        val data = HashMap<String, Any>()
         val pickupAddress = context.queryParam("pickupAddress")
         val dropOffAddress = context.queryParam("dropOffAddress")
         val time = context.queryParam("time")
@@ -20,11 +20,7 @@ class CreateNewParcel: Handler {
             val firebaseAuth = FirebaseAuth.getInstance()
             try {
                 firebaseAuth.getUser(uid)
-                data["dropOff"] = dropOffAddress
-                data["pickup"] = pickupAddress
-                data["time"] = time
-                data["userId"] = uid
-                docRef.document().set(data)
+                docRef.document().create(Parcel(uid, pickupAddress, dropOffAddress, time))
                 context.res.status = 200
             } catch (exception: Exception){
                 context.res.status = 401
