dc64dd3f445971a45d761a5d556ae831ca4fd32c
s3790768 <s3790768@student.rmit.edu.au>
2021-01-15 16:46:20 +1100

    Check for unauthorised user when adding data

--- src/backend/src/main/java/controllers/CreateNewParcel.kt
+++ src/backend/src/main/java/controllers/CreateNewParcel.kt
@@ -1,8 +1,10 @@
 package controllers
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.cloud.FirestoreClient
 import io.javalin.http.Context
 import io.javalin.http.Handler
+import java.lang.Exception
 
 class CreateNewParcel: Handler {
 
@@ -15,14 +17,22 @@ class CreateNewParcel: Handler {
         val time = context.queryParam("time")
         val uid = context.queryParam("userId")
         if(pickupAddress != null && dropOffAddress != null && time != null && uid != null){
-            data["dropOff"] = dropOffAddress
-            data["pickup"] = pickupAddress
-            data["time"] = time
-            data["userId"] = uid
-            docRef.document().set(data)
-            context.res.status = 200
+            val firebaseAuth = FirebaseAuth.getInstance()
+            try {
+                firebaseAuth.getUser(uid)
+                data["dropOff"] = dropOffAddress
+                data["pickup"] = pickupAddress
+                data["time"] = time
+                data["userId"] = uid
+                docRef.document().set(data)
+                context.res.status = 200
+            } catch (exception: Exception){
+                context.res.status = 401
+                context.result("Unauthorized user")
+            }
+
         } else {
-            context.res.status = 500
+            context.result("Invalid data")
         }
     }
 }
