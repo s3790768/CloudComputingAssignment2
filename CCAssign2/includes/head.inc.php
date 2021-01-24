feff0ff5a40712a4d1ee47bdb5f56f875e6d9f63
Musfiqur Rahman <s3703103@student.rmit.edu.au>
2021-01-24 19:41:37 +1100

    added checkout.html-not completed

--- CCAssign2/includes/head.inc.php
+++ CCAssign2/includes/head.inc.php
@@ -22,6 +22,9 @@
 <script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-auth.js"></script>
 <script src="https://www.gstatic.com/firebasejs/ui/4.6.1/firebase-ui-auth.js"></script>
+
+
+
 <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.6.1/firebase-ui-auth.css" />
 
 <link href="style.css" rel="stylesheet">
