f6614bef0a0b36b89044db83206d22df41aad830
s3790768 <s3790768@student.rmit.edu.au>
2021-01-24 18:14:23 +1100

    attempt to submit data to backend

--- CCAssign2/scripts/auth.js
+++ CCAssign2/scripts/auth.js
@@ -23,3 +23,17 @@ var uiConfig = {
     ]
 };
 auth.start('#auth-container', uiConfig);
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log(user.id)
+        setCookie('userId', user.uid)
+    }
+});
+
+function setCookie(name,value) {
+    var expires = "";
+    var date = new Date();
+    date.setTime(date.getTime() + (24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
